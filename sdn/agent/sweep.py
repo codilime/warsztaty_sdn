71597545c864253383708b018df9cfe18d1c38c4
Piotr Kozłowski <piotr.kozlowski@ntti3.com>
2018-04-25 11:57:37 +0200

    Use ping for reachability

--- sdn/agent/sweep.py
+++ sdn/agent/sweep.py
@@ -6,26 +6,10 @@ logger = logging.getLogger(__name__)
 
 
 class PingSweep(object):
-    SWEEP_CMD = 'nmap -v -sn -PE -n -oG - %s'
-    HOSTS_UP = re.compile(r"([0-9]+) host(s)? up")
-
-    def sweep(self, target):
-        logger.info("Starting network sweep on %s", target)
-        output = self.exec_command(self.SWEEP_CMD % target)
-
-        logger.debug("Sweep result: %s", output)
-        if not output:
-            return 0
-
-        match = self.HOSTS_UP.search(output)
-        if not match:
-            return 0
-
-        if len(match.groups()) > 0:
-            return int(match.groups()[0])
-        else:
-            return 0
-
     @staticmethod
-    def exec_command(cmd):
-        return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, close_fds=True).stdout.read().decode('utf-8')
+    def sweep(target):
+        logger.info("Checking availability of %s", target)
+        result = subprocess.run(['ping', '-c1', target])
+        logging.debug('Stdout: %s', result.stdout)
+        logging.debug('Stderr: %s', result.stderr)
+        return 1 if result.returncode == 0 else 0
