895f29c7e432c17b67f52568df3dff80acd0b345
pawelkopka <pawel.kopka22@gmail.com>
2018-07-31 13:12:25 +0200

    change name

--- sdn/controller/controller.py
+++ sdn/controller/controller.py
@@ -17,20 +17,20 @@ class Controller(object):
         self.ipam_pools = {}
         self.networks = {}
 
-    def add_network(self, n):
-        logger.info("Adding network %s", n.ip)
-        subnets = netaddr.IPNetwork(n.ip).subnet(29)
-        self.ipam_pools[n.id] = itertools.islice(subnets, 2) # we've got at most 2 subnets
+    def add_network(self, network):
+        logger.info("Adding network %s", network.ip)
+        subnets = netaddr.IPNetwork(network.ip).subnet(29)
+        self.ipam_pools[network.id] = itertools.islice(subnets, 2) # we've got at most 2 subnets
 
-        self.router.add_network(n)
-        self.networks[n.id] = n
+        self.router.add_network(network)
+        self.networks[network.id] = network
 
     def get_network(self, id):
         return self.networks[id]
 
-    def add_logical_port(self, p):
-        logger.info("Adding logical port on %s for %s", p.network.id, p.container.id)
-        pool = next(self.ipam_pools[p.network.id])
+    def add_logical_port(self, port):
+        logger.info("Adding logical port on %s for %s", port.network.id, port.container.id)
+        pool = next(self.ipam_pools[port.network.id])
         hosts = pool.iter_hosts()
         next(hosts) #skip docker default gateway
         router_ip, container_ip = next(hosts), next(hosts)
@@ -38,13 +38,13 @@ class Controller(object):
 
         logger.debug("Creating docker networks")
         ipam = docker.types.IPAMConfig(pool_configs=[docker.types.IPAMPool(subnet=str(pool))])
-        docker_net = self.docker_client.networks.create(p.network.id, driver="bridge", ipam=ipam)
+        docker_net = self.docker_client.networks.create(port.network.id, driver="bridge", ipam=ipam)
         docker_net.connect(self.router.id, ipv4_address=router_ip.format())
-        docker_net.connect(p.container.id, ipv4_address=container_ip.format())
+        docker_net.connect(port.container.id, ipv4_address=container_ip.format())
 
         logger.debug("Notifying router and container")
-        p.router_ip = router_ip
-        p.container_ip = container_ip
-        p.underlay_network_ip = self.get_network(p.network.id).ip
-        self.router.add_logical_port(p)
-        p.container.add_logical_port(p)
+        port.router_ip = router_ip
+        port.container_ip = container_ip
+        port.underlay_network_ip = self.get_network(port.network.id).ip
+        self.router.add_logical_port(port)
+        port.container.add_logical_port(port)
