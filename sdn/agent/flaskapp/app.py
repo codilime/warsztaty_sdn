a645214625362dbe76a8b7c904008b52b54fe4ce
pawelkopka <pawel.kopka22@gmail.com>
2018-08-01 14:10:59 +0200

    add unitest to agent/flaskapp

--- sdn/agent/flaskapp/app.py
+++ sdn/agent/flaskapp/app.py
@@ -1,14 +1,19 @@
 import logging
 from flask import Flask, request
-from agent.sweep import PingSweep
-from agent.logical_port import LogicalPort, CommandExecutor
-from config.flask_config import get_config
+from sdn.agent.sweep import PingSweep
+from sdn.agent.logical_port import LogicalPort, CommandExecutor
+from sdn.config.flask_config import get_config
 
 
 app = Flask('Agent')
 config = get_config()
 debug_mode = config.getboolean('agent', 'debug')
 logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return str('hello')
 
 
 @app.route('/ping/<path:target>', methods=['GET'])
@@ -19,15 +24,22 @@ def ping_target(target):
 
 @app.route('/create/logical_port', methods=['POST'])
 def create_logical_port():
-    data = request.get_json()
-    logging.debug("Received %s", str(data))
-    new_lp = LogicalPort(net=data.get('net'),
-                         net_ip=data.get('net_ip'),
-                         router_ip=data.get('router_ip'),
-                         local_ip=data.get('ip'))
-    new_lp.create(CommandExecutor())
-    return 'Success\n'
 
-app.run(config.get('agent','listen_address'),
-        config.getint('agent', 'listen_port'),
-        debug_mode)
+    data = request.get_json()
+    print(data)
+    logging.debug("Received %s", str(data))
+    try:
+        new_lp = LogicalPort(net=data.get('net'),
+                             net_ip=data.get('net_ip'),
+                             router_ip=data.get('router_ip'),
+                             local_ip=data.get('ip'))
+        new_lp.create(CommandExecutor())
+        return 'Success\n', 200
+    except TypeError:
+        return 'Failed, not enough data\n', 400
+
+
+if __name__ == '__main__':
+    app.run(config.get('agent', 'listen_address'),
+            config.getint('agent', 'listen_port'),
+            debug_mode)
---
+++ sdn/agent/flaskapp/tests/test_app.py
@@ -0,0 +1,48 @@
+import unittest
+import json
+from ..app import app
+
+
+class TestAgentFlaskaap(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_hello(self):
+        """Start with a blank database."""
+        rv = self.client.get('/hello')
+        self.assertEqual(b'hello', rv.data)
+        self.assertEqual(200, rv.status_code)
+
+    def test_ping_target_8888(self):
+        """Start with a blank database."""
+        rv = self.client.get('/ping/8.8.8.8')
+        self.assertEqual(b'1', rv.data)
+        self.assertEqual(200, rv.status_code)
+
+    def test_ping_target_12700257(self):
+        """Start with a blank database."""
+        rv = self.client.get('/ping/127.0.0.257')
+        self.assertEqual(b'0', rv.data)
+        self.assertEqual(200, rv.status_code)
+
+    def test_create_logical_port(self):
+        """Start with a blank database."""
+        data = {
+            'net': 'ala',
+            'net_ip' : '192.168.0.0/24',
+            'router_ip': '10.0.0.1',
+            'local_ip': '192.168.0.11'
+        }
+        rv = self.client.post('/create/logical_port', data=json.dumps(data),
+                              content_type='application/json')
+        self.assertEqual(b'Success\n', rv.data)
+        self.assertEqual(200, rv.status_code)
+
+    def test_create_logical_port_without_data(self):
+        """Start with a blank database."""
+        data = {}
+        rv = self.client.post('/create/logical_port', data=json.dumps(data),
+                              content_type='application/json')
+        print(rv.data)
+        self.assertEqual(b'Failed, not enough data\n', rv.data)
+        self.assertEqual(400, rv.status_code)
