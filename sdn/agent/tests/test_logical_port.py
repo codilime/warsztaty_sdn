7e9d01d9253d14d94d7f94e92b30962496ed409a
Piotr Kozłowski <piotr.kozlowski@ntti3.io>
2018-08-23 22:28:30 +0200

    Agent: logic for logical port deletion

--- sdn/agent/tests/test_logical_port.py
+++ sdn/agent/tests/test_logical_port.py
@@ -19,6 +19,15 @@ class LogicalPortTest(unittest.TestCase):
 
         self.assertListEqual(mock_cmd.commands[0], ["ip", "route", "add", "192.168.0.0/24", "via", "192.168.0.1"])
 
+    def test_should_remove_default_route(self):
+        mock_cmd = MockCmdExecutor()
+        p = LogicalPort("net1", "192.168.0.0/24", "192.168.0.1", "192.168.0.2")
+
+        p.create(mock_cmd)
+        p.delete(mock_cmd)
+
+        self.assertListEqual(mock_cmd.commands[1], ["ip", "route", "del", "192.168.0.0/24", "via", "192.168.0.1"])
+
 
 if __name__ == '__main__':
     unittest.main()
