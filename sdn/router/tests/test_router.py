cf04c8124bfdf7fc3982baa90950ef1a4009e45f
Piotr Kozłowski <piotr.kozlowski@ntti3.io>
2018-08-25 09:47:40 +0200

    Rozwiazanie 9

--- sdn/router/tests/test_router.py
+++ sdn/router/tests/test_router.py
@@ -33,21 +33,6 @@ class RouterTest(unittest.TestCase):
         self.assertListEqual(cmds.commands[1],
                              ['iptables', '-A', 'FORWARD', '-i', 'eth1', '-o', 'eth0', '-j', 'ACCEPT'])
 
-    def test_should_remove_logical_port(self):
-        cmds = MockCmdExecutor()
-        interfaces = MockInterfaceFinder({"10.0.0.1": "eth0", "10.0.1.1": "eth1"})
-        r = Router(cmds, interfaces)
-
-        r.add_network("net1")
-        r.add_logical_port("net1", "10.0.0.1")
-        r.add_logical_port("net1", "10.0.1.1")
-
-        r.remove_logical_port("net1", "10.0.0.1")
-
-        self.assertListEqual(cmds.commands[2],
-                             ['iptables', '-D', 'FORWARD', '-i', 'eth1', '-o', 'eth0', '-j', 'ACCEPT'])
-        self.assertListEqual(cmds.commands[3],
-                             ['iptables', '-D', 'FORWARD', '-i', 'eth0', '-o', 'eth1', '-j', 'ACCEPT'])
 
 if __name__ == '__main__':
     unittest.main()
